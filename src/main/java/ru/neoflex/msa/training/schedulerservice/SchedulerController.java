3b2c125ce30b7e55a4c0a9868cc0becfb95b6c77
Anton Talanov <dotollia@mail.ru>
2021-01-25 00:54:58 +0400

    add application properties

--- src/main/java/ru/neoflex/msa/training/schedulerservice/SchedulerController.java
+++ src/main/java/ru/neoflex/msa/training/schedulerservice/SchedulerController.java
@@ -1,21 +1,20 @@
 package ru.neoflex.msa.training.schedulerservice;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
 @Component
 public class SchedulerController {
-    private static final Long INTERVAL = 5000L;
+    private static final int INTERVAL = 5000;
 
-    private static final String HOST = "10.20.30.11";
-    private static final String PORT = "8080";
-    private static final String PATH = "";
+    @Value("${application.scheduler.invoke.url}")
+    private String URL;
 
-    @Scheduled(fixedRate = 5000)
+    @Scheduled(fixedRate = INTERVAL)
     public void invokeService() {
         RestTemplate restTemplate = new RestTemplate();
-        BankAccount bankAccount = restTemplate.getForObject("http://" + HOST + ":" + PORT + PATH  + "/", BankAccount.class);
-        System.out.println(bankAccount);
+        Object object = restTemplate.getForObject("http://" + URL, Object.class);
     }
 }
